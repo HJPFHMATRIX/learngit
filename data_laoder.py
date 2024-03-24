3b446985fa7c86534f9b29fd6af872bb19c597b1
JPF <1544190197@qq.com>
2024-03-24 20:24:01 +0800

    20:23

--- data_laoder.py
+++ data_laoder.py
@@ -1,9 +1,9 @@
-import os
-from PIL import Image
+# import os
+# from PIL import Image
 from torch.utils.data import DataLoader
-from torch.utils.data import Dataset
-import matplotlib.pyplot as plt
-import torch
+# from torch.utils.data import Dataset
+# import matplotlib.pyplot as plt
+# import torch
 import torchvision
 
 
@@ -19,18 +19,18 @@ dateset = torchvision.datasets.MNIST('mnist_data', train=True, download=True,
                                ])),  # 下载数据集,用于训练
 
 
-dateset_2 = torchvision.datasets.CIFAR10('cifar10_data/', train=False, download=True,
-                               transform=torchvision.transforms.Compose([
-                                   torchvision.transforms.ToTensor(),  # 将数据转换为tensor格式,下载的数据是numpy格式
-                                   torchvision.transforms.Normalize((0.1307,), (0.3081,))
-                                   # 正则化数据，神经网络的数据最好在0附近均匀分布，但是图片的参数是在0到1之间，所以需要正则化数据(提高性能)
-                               ])),  # 下载数据集,用于测试
+# dateset_2 = torchvision.datasets.CIFAR10('cifar10_data/', train=False, download=True,
+#                                transform=torchvision.transforms.Compose([
+#                                    torchvision.transforms.ToTensor(),  # 将数据转换为tensor格式,下载的数据是numpy格式
+#                                    torchvision.transforms.Normalize((0.1307,), (0.3081,))
+#                                    # 正则化数据，神经网络的数据最好在0附近均匀分布，但是图片的参数是在0到1之间，所以需要正则化数据(提高性能)
+#                                ])),  # 下载数据集,用于测试
 
 dataloader = DataLoader(dateset, batch_size=64, shuffle=True, num_workers=2, drop_last=False)
-dataloader_2 = DataLoader(dateset_2, batch_size=64, shuffle=True, num_workers=2, drop_last=False)
+# dataloader_2 = DataLoader(dateset_2, batch_size=64, shuffle=True, num_workers=2, drop_last=False)
 
 
 
 print(dateset)
 print(dataloader)
-print(dateset_2)
+# print(dateset_2)
