14ee11cc82d788c20116490cddbcce19a39bf69b
JPF <1544190197@qq.com>
2024-03-24 20:21:07 +0800

    20:21

--- data_laoder.py
+++ data_laoder.py
@@ -3,28 +3,34 @@ from PIL import Image
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 import matplotlib.pyplot as plt
-
-class my_dataset(Dataset):
-    def __init__(self, data_address, label_address):
-        self.data = data_address
-        self.label = label_address
-        self.images = os.listdir(data_address)
-    def __getitem__(self, index):
-        image_item = self.images[index]
-        img = plt.imread(os.path.join(self.data, image_item))
-        return img
-
-    def __len__(self):
-        return len(self.data)
+import torch
+import torchvision
 
 
-# 创建一个数据集
 
 
-train_data = my_dataset(r'C:\Users\Administrator\Desktop\images',
-                        r'C:\Users\Administrator\Desktop\lables')
-# 创建一个dataloader,设置批大小为64，每一个epoch重新洗牌不舍弃不能被整除的批次
-train_loader = DataLoader(dataset=train_data, batch_size=64, shuffle=True, num_workers=2, drop_last=False)
 
-print(train_loader.dataset.data)
-print("get_item",train_data.__getitem__(0))
+
+dateset = torchvision.datasets.MNIST('mnist_data', train=True, download=True,
+                               transform=torchvision.transforms.Compose([
+                                   torchvision.transforms.ToTensor(),  # 将数据转换为tensor格式,下载的数据是numpy格式
+                                   torchvision.transforms.Normalize((0.1307,), (0.3081,))
+                                   # 正则化数据，神经网络的数据最好在0附近均匀分布，但是图片的参数是在0到1之间，所以需要正则化数据(提高性能)
+                               ])),  # 下载数据集,用于训练
+
+
+dateset_2 = torchvision.datasets.CIFAR10('cifar10_data/', train=False, download=True,
+                               transform=torchvision.transforms.Compose([
+                                   torchvision.transforms.ToTensor(),  # 将数据转换为tensor格式,下载的数据是numpy格式
+                                   torchvision.transforms.Normalize((0.1307,), (0.3081,))
+                                   # 正则化数据，神经网络的数据最好在0附近均匀分布，但是图片的参数是在0到1之间，所以需要正则化数据(提高性能)
+                               ])),  # 下载数据集,用于测试
+
+dataloader = DataLoader(dateset, batch_size=64, shuffle=True, num_workers=2, drop_last=False)
+dataloader_2 = DataLoader(dateset_2, batch_size=64, shuffle=True, num_workers=2, drop_last=False)
+
+
+
+print(dateset)
+print(dataloader)
+print(dateset_2)
